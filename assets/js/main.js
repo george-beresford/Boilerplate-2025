0a9989bc33dc733bbc6df56631bb4d65293c5e60
George Beresford <github@georgeberesford.co.uk>
2025-04-30 18:59:06 +0100

    Implement hamburger menu expand/collapse logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,27 @@
-(function($){
-    $(document).ready(() => {
-        console.info("jQuery loaded");
-    });
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleMenu = (event) => {
+        event.preventDefault();
+        const trigger = event.currentTarget;
+        const targetId = trigger.getAttribute("href").substring(1); // Get the ID from href
+        const targetElement = document.getElementById(targetId);
+
+        if(!targetElement) { console.warn(targetId + " wasn't found in the DOM."); return; }
+
+        if (trigger.dataset.action === "will-open") {
+            trigger.dataset.action = "will-close";
+            trigger.setAttribute("title", "Expand hidden navigation menu");
+            targetElement.classList.add("open");
+            targetElement.classList.remove("close");
+        } else {
+            trigger.dataset.action = "will-open";
+            trigger.setAttribute("title", "Collapse visible navigation menu");
+            targetElement.classList.add("close");
+            targetElement.classList.remove("open");
+        }
+    };
+
+    const menuTrigger = document.querySelector('[data-role="hamburger-menu"]');
+    if (menuTrigger) {
+        menuTrigger.addEventListener("click", toggleMenu);
+    }
 });
--- includes/nav.php
+++ includes/nav.php
@@ -2,21 +2,44 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <a href="" title="">
-                    <img id="brand" src="" alt="" class="object-fit-contain">
+                <a href="" title="<?= $config['title']['brand_name']; ?>">
+                    <img id="brand" src="assets/svg/logo.svg" alt="<?= $config['title']['brand_name']; ?>" class="object-fit-contain">
+                </a>
+            </div>
+            <div class="col visible-xs visible-sm">
+                <a href="#hamburger-menu" data-role="hamburger-menu" data-action="open" title="Expand menu">
+                    <span class="visually-hidden">Expand menu</span>
+                    <icon id="open">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                            <polyline points="4,10 44,10"></polyline>
+                            <polyline points="4,24 44,24"></polyline>
+                            <polyline points="4,38 44,38"></polyline>
+                        </svg>
+                    </icon>
+                    <icon id="close">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                            <polyline points="4,44 24,24 44,44"></polyline>
+                            <polyline points="44,4 24,24 4,4"></polyline>
+                        </svg>
+                    </icon>
                 </a>
             </div>
         </div>
     </div>
-    <nav>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <ul class="list-unstyled d-flex flex-row">
-                        <li><a href="" title="Link">Link</a></li>
-                    </ul>
-                </div>
+</header>
+<nav id="hamburger-menu">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <ul class="list-unstyled d-flex flex-column flex-lg-row">
+                    <li><a href="" title="Link">Link</a></li>
+                    <li><a href="" title="Link">Link</a></li>
+                    <li><a href="" title="Link">Link</a></li>
+                    <li><a href="" title="Link">Link</a></li>
+                    <li><a href="" title="Link">Link</a></li>
+                    <li><a href="" title="Link">Link</a></li>
+                </ul>
             </div>
         </div>
-    </nav>
-</header>
+    </div>
+</nav>
